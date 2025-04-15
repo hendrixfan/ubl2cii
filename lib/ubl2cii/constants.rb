3999659a393e93a6a8297ddbe4474342361746b6
Wolfgang <w.wohanka@gmail.com>
2025-04-15 20:24:42 +0200

    Update constants.rb

--- lib/ubl2cii/constants.rb
+++ lib/ubl2cii/constants.rb
@@ -8,19 +8,19 @@ module Ubl2Cii
   FORMATABLE_ELEMENTS = %w[DateTimeString].freeze
 
   # XML Namespace URIs
-  NAMESPACE_RSM = 'urn:un:unece:uncefact:data:standard:CrossIndustryInvoice:100'
-  NAMESPACE_RAM = 'urn:un:unece:uncefact:data:standard:ReusableAggregateBusinessInformationEntity:100'
-  NAMESPACE_UDT = 'urn:un:unece:uncefact:data:standard:UnqualifiedDataType:100'
-  NAMESPACE_QDT = 'urn:un:unece:uncefact:data:standard:QualifiedDataType:100'
-  NAMESPACE_CBC = 'urn:oasis:names:specification:ubl:schema:xsd:CommonBasicComponents-2'
-  NAMESPACE_CAC = 'urn:oasis:names:specification:ubl:schema:xsd:CommonAggregateComponents-2'
+  NAMESPACE_RSM = 'urn:un:unece:uncefact:data:standard:CrossIndustryInvoice:100'.freeze
+  NAMESPACE_RAM = 'urn:un:unece:uncefact:data:standard:ReusableAggregateBusinessInformationEntity:100'.freeze
+  NAMESPACE_UDT = 'urn:un:unece:uncefact:data:standard:UnqualifiedDataType:100'.freeze
+  NAMESPACE_QDT = 'urn:un:unece:uncefact:data:standard:QualifiedDataType:100'.freeze
+  NAMESPACE_CBC = 'urn:oasis:names:specification:ubl:schema:xsd:CommonBasicComponents-2'.freeze
+  NAMESPACE_CAC = 'urn:oasis:names:specification:ubl:schema:xsd:CommonAggregateComponents-2'.freeze
 
-  PREFIX_RSM = 'rsm'
-  PREFIX_RAM = 'ram'
-  PREFIX_UDT = 'udt'
-  PREFIX_CBC = 'cbc'
-  PREFIX_CAC = 'cac'
-  PREFIX_QDT = 'qdt'
+  PREFIX_RSM = 'rsm'.freeze
+  PREFIX_RAM = 'ram'.freeze
+  PREFIX_UDT = 'udt'.freeze
+  PREFIX_CBC = 'cbc'.freeze
+  PREFIX_CAC = 'cac'.freeze
+  PREFIX_QDT = 'qdt'.freeze
 
-  OUTPUT_ENCODING = 'UTF-8'
+  OUTPUT_ENCODING = 'UTF-8'.freeze
 end
